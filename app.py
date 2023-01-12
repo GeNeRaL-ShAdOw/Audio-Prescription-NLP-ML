a1feca172735172ecd869f98b86b09ac3634d103
MilindPruthi09 <milindpruthi09@gmail.com>
2023-01-12 17:02:31 +0530

    initial upload

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
 @app.route('/UI', methods=['GET','POST'])
 def UI():
     return render_template("ui.html")
+
+@app.route('/audioRecog', methods=['GET','POST'])
+def audioRecog():
+    print("audioRecog")
+    
+    return "I"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/src/ui.js
+++ static/src/ui.js
@@ -14,10 +14,11 @@ function handlerFunction(stream) {
                 type: 'audio/mp3'
             });
             recordedAudio.src = URL.createObjectURL(blob);
-            alert("This is audio:",(blob));
+            alert("This is audio:", (blob));
             recordedAudio.controls = true;
             recordedAudio.autoplay = true;
             sendData(blob)
+            sendAudio(blob)
         }
     }
 }
@@ -25,7 +26,7 @@ function handlerFunction(stream) {
 function sendData(data) {}
 record.onclick = e => {
     record.disabled = true;
-    record.style.backgroundColor = "blue"
+    // record.style.backgroundColor = "blue"
     stopRecord.disabled = false;
     audioChunks = [];
     rec.start();
@@ -33,6 +34,40 @@ record.onclick = e => {
 stopRecord.onclick = e => {
     record.disabled = false;
     stop.disabled = true;
-    record.style.backgroundColor = "red"
+    // record.style.backgroundColor = "red"
     rec.stop();
-    }
+}
+
+// function uploadAudio(blob) {
+//     let data = new FormData();
+//     data.append('file', audioBlob);
+//     return axios
+//         .post("http://127.0.0.1:5000/audioRecog", data, {
+//             headers: {
+//                 'Content-Type': 'multipart/form-data',
+//             },
+//         })
+//         .then(res => {
+//             console.log(res)
+//             return res
+//         });
+// }
+
+function sendAudio(blob){
+    var wavFile = new File([ blob ], "audio.wav");   
+    var form    = new FormData();
+    form.append("myAudio", wavFile);
+
+    $.ajax(
+        {
+            url: "http://127.0.0.1:5000/audioRecog",
+            type: "POST",
+            data: form,
+            contentType: false,
+            processData: false,
+            success: function(getData)
+            {
+                console.log(getData);
+            }
+        });
+}
